443062810828b6d3898b9f04163e83f1de113f07
abdqam <aboodq93@gmail.com>
2021-02-09 22:08:09 +0200

    Product manager 1

---
+++ MERNStack/FullStackMern/ProductManager1/server/routes/product.routes.js
@@ -0,0 +1,6 @@
+const productController=require("../controllers/product.controller");
+module.exports=function(app){
+    app.get('/api',productController.index);
+    app.post('/api/products', productController.createProduct);
+
+}
