f50977d9b4b3a1f4311628b474bf19e28b3451e8
abdqam <aboodq93@gmail.com>
2021-02-14 20:53:04 +0200

    Authors

---
+++ MERNStack/AdvancedMern/Authors/server/models/author.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const AuthorSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true, "Auhor's name must be present"],
+        minlength: [3, "Name must be at least 3  characters!"],
+        maxlength: [600, "Name must be less than 50 characters!"]
+    }
+},{timestamps:true})
+module.exports.Author = mongoose.model("Author",AuthorSchema);
